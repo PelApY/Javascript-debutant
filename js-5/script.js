6d8e54dd84a70d82566067b612e1ee98d04f2423
PelApY <anthony.pelong@gmail.com>
2024-07-11 22:13:04 +0200

    todo list js

---
+++ js-5/script.js
@@ -0,0 +1,26 @@
+const newTaskInput = document.getElementById('new-task');
+const addTaskButton = document.getElementById('add-task');
+const taskList = document.getElementById('task-list');
+
+addTaskButton.addEventListener('click', function () {
+    const taskText = newTaskInput.value;
+    const taskItem = document.createElement('li');
+    const taskCheckbox = document.createElement('input');
+    const taskLabel = document.createElement('label');
+
+    taskCheckbox.type = 'checkbox';
+    taskLabel.innerText = taskText;
+
+    taskItem.appendChild(taskCheckbox);
+    taskItem.appendChild(taskLabel);
+    taskList.appendChild(taskItem);
+
+    newTaskInput.value = '';
+});
+
+taskList.addEventListener('change', function(event) {
+    if (event.target.type === 'checkbox') {
+      const taskItem = event.target.parentElement;
+      taskItem.classList.toggle('completed');
+    }
+  });
